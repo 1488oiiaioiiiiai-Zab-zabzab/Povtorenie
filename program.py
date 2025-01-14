ede8b9068198212ed36e9ef9a2a78892ba1d793a
Yura <Cheremenskii.yuri@gmail.com>
2025-01-14 22:57:36 +0300

    need more changes

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 def main():
     print('This is my first git program')
     print('and I change it every day')
+    print('I NEEEEED MOOOOOOOOOOOOOOOOOOOOOOOORE CHANGEEEEES')
 
 
 if __name__ == '__main__':
