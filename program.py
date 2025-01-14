6f05c250378ad5b35a24630166416335a3cb45c1
Yuri <Cheremenskii.yuri@gmail.com>
2025-01-14 23:03:40 +0300

    this commit is to pull changes from remote repository to local

--- program.py
+++ program.py
@@ -2,6 +2,7 @@ def main():
     print('This is my first git program')
     print('and I change it every day')
     print('I NEEEEED MOOOOOOOOOOOOOOOOOOOOOOOORE CHANGEEEEES')
+    print('this print i am writing from GitHub!')
 
 
 if __name__ == '__main__':
